e8a33832b7a2a0c3059f5a4a7e4092dfb18934ee
sunghyuki <sunghyuk1609@gmail.com>
2021-01-06 18:37:57 +0900

    add: 푸시알림 어플 추가

---
+++ MyNotificationApp2/app/src/main/java/com/example/mynotificationapp2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mynotificationapp2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button button;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get the token
+                String token = FirebaseInstanceId.getInstance().getToken();
+                Log.d(TAG, "Token: " + token);
+                editText.setText(token);
+            }
+        });
+
+        Intent intent = getIntent();
+        if(intent != null) {//푸시알림을 선택해서 실행한것이 아닌경우 예외처리
+            String notificationData = intent.getStringExtra("test");
+            if(notificationData != null)
+                Log.d(TAG, notificationData);
+        }
+    }
+}
